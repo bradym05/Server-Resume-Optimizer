c6ab282e153e0306c731d4051f7d47a0224d4802
Brady Maki <78286012+bradym05@users.noreply.github.com>
2025-04-22 03:53:48 -0600

    In-depth comparison
    Updated optimize_resume to perform a more nuanced comparison, with hyperparameters tweaked for the best performance.

--- optimize_resume.py
+++ optimize_resume.py
@@ -1,14 +1,14 @@
 from docx import Document
 from enum import Enum
-from typing import Final, Tuple
+from typing import Final, Tuple, Dict
 from rakun2 import RakunKeyphraseDetector
 
 # Hyperparemeters for rakun2
 RAKUN_HYPERPARAMETERS: Final = {
-    "num_keywords": 20,
-    "merge_threshold": 1.1,
-    "alpha": 0.3,
-    "token_prune_len": 3
+    "num_keywords": 70,
+    "merge_threshold": 0.5,
+    "alpha": 0.9,
+    "token_prune_len": 6
 }
 
 class ResumeOptimizer():
@@ -16,41 +16,46 @@ class ResumeOptimizer():
     Resume Class - Initialize from a valid python-docx Document object
     """
     def __init__(self, resume_doc: Document, job_description: str):
+        # Set attributes
         self.resume_doc = resume_doc
-        self.resume_text = "\n".join([p.text for p in self.resume_doc.paragraphs])
-        self.job_description = job_description
-        self.__keyword_detector = RakunKeyphraseDetector(RAKUN_HYPERPARAMETERS)
-        self.__keyword_cache = {}
+        self.job_string = job_description
+        self.resume_string = "\n".join([p.text for p in self.resume_doc.paragraphs])
+        # Keywords objects
+        self.resume_keywords = RakunKeyphraseDetector(RAKUN_HYPERPARAMETERS)
+        self.job_keywords = RakunKeyphraseDetector(RAKUN_HYPERPARAMETERS)
 
-    def get_keywords(self, attribute) -> list[Tuple[str, float]]:
+    def get_keywords(self, rakun_object:RakunKeyphraseDetector, text:str ) -> list[Tuple[str, float]]:
         """
-        Extract keywords of given attribute (doc name) with rakun2, cache results
-        Args:
-        attribute - ("resume_text" or "job_description")
+        Get final keywords from rakun2 object with given text
+        Use pre-existing results if possible
         """
-        # Get attribute
-        keyword_text = getattr(self, attribute, False)
-        if keyword_text and len(keyword_text) > 0:
-            # Check cache
-            if not AttributeError in self.__keyword_cache.keys():
-                # Find keywords and update cache
-                self.__keyword_cache[attribute] = self.__keyword_detector.find_keywords(keyword_text, input_type="string")
-            # Return cached keywords
-            return self.__keyword_cache[attribute]
+        if rakun_object.final_keywords:
+            return rakun_object.final_keywords
+        else:
+            return rakun_object.find_keywords(text, input_type="string")
         
-    def compare_keywords(self):
+    def compare_keywords(self) -> Tuple[Dict[str, Dict[str, float]], list[Tuple[str, float]], float]:
         """
         Compare the scores of resume_text and job_description
+        Return values:
+            Match Dictionary {
+                Keyword: {
+                    Difference - Float representing the difference between docs
+                    Value - Float representing the keyword's value in the job posting
+                }
+            }
+            Missed Words - List of missed words sorted from highest to lowest value
+            Total Difference - Float sum of total difference between docs
         """
         # Get keywords
-        job_results = self.get_keywords("job_description")
-        resume_results = self.get_keywords("resume_text")
+        job_results = self.get_keywords(self.job_keywords, self.job_string)
+        resume_results = self.get_keywords(self.resume_keywords, self.resume_string)
         # Create list of job keywords only
         job_keywords = [keyword_tuple[0] for keyword_tuple in job_results]
         # Results variables
-        total_matches = 0
+        matches = {}
+        missed_words = []
         total_difference = 0
-        match_differences = {}
         # Iterate over resume keywords
         for keyword_tuple in resume_results:
             # Check for a match
@@ -58,16 +63,31 @@ class ResumeOptimizer():
             if resume_keyword in job_keywords:
                 # Get matched index
                 matched_index = job_keywords.index(resume_keyword)
-                job_keywords.pop(matched_index)
                 # Get full tuple
-                job_tuple = job_results[matched_index + total_matches]
+                job_tuple = job_results[matched_index]
                 # Record match difference
                 difference = job_tuple[1] - keyword_tuple[1]
                 total_difference += abs(difference)
-                match_differences[resume_keyword] = difference
-                # Update total matches
-                total_matches += 1
-        print(f"Keyword Matches: {total_matches}")
+                matches[resume_keyword] = {
+                    "Difference": difference,
+                    "Value": job_tuple[1]
+                }
+        # Iterate over unmatched keywords from job posting
+        for index in range(len(job_keywords)):
+            # Check if keyword matched
+            keyword = job_keywords[index]
+            if keyword in matches.keys():
+                continue
+            else:
+                # Update difference
+                total_difference += job_results[index][1]
+                missed_words.append(job_results[index])
+        # Sort missed words
+        missed_words.sort(key=lambda keyword_tuple: keyword_tuple[1], reverse=True)
         print(f"Usage Difference {total_difference}")
-        for keyword, difference in match_differences.items():
-            print(f'Keyword "{keyword}" value difference: {difference}')
+        for keyword, info in matches.items():
+            print(f'Keyword "{keyword}" value difference: {info["Difference"]}')
+        print("Resume Keywords", [keyword_tuple[0] for keyword_tuple in resume_results])
+        print("Job Keywords", job_keywords)
+        print(missed_words)
+        return matches, missed_words, total_difference
