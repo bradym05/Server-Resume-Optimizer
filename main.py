d387e6eb82d158ce853f63c268401ca0c14ed92f
Brady Maki <78286012+bradym05@users.noreply.github.com>
2025-04-21 23:06:49 -0600

    CORS
    Allowed origin of my github pages website

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+# List of allowed origins
+origins = [
+    "https://bradym05.github.io/Resume-Optimizer/",
+]
 
 # Create FastAPI App Object
 app = FastAPI()
+
+# Add list of origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Main upload function
 @app.post("/uploadfile/")
