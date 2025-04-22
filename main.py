f76fbd0db41cfe8fcb01234233a25549a2c34a00
Brady Maki <78286012+bradym05@users.noreply.github.com>
2025-04-22 01:44:03 -0600

    Basic Keyword Comparison
    Compare keywords from job description and resume upon file submission

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from io import BytesIO
 
 from docx import Document
 
-from fastapi import FastAPI, UploadFile, HTTPException
+from typing import Annotated, Final
+
+from fastapi import FastAPI, UploadFile, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 
-from optimize_resume import Resume
+from optimize_resume import ResumeOptimizer
 
 # List of allowed origins
-origins = [
+ALLOWED_ORIGINS: Final = [
     "https://bradym05.github.io",
 ]
+# Max job description length
+JOB_DESCRIPTION_MAX_LENGTH: Final = 2000
 
 # Create FastAPI App Object
 app = FastAPI()
@@ -18,7 +22,7 @@ app = FastAPI()
 # Add list of origins
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=ALLOWED_ORIGINS,
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -26,14 +30,14 @@ app.add_middleware(
 
 # Main upload function
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
+async def create_upload_file(file: UploadFile, job_description: Annotated[str, Query(max_length=JOB_DESCRIPTION_MAX_LENGTH)]):
     # Validate file type
     if file.filename.endswith(".docx"):
         # Get file contents
         contents = await file.read()
         # Create resume
-        resume_object = Resume(Document(BytesIO(contents)))
-        print(resume_object.get_key_words())
+        resume_object = ResumeOptimizer(Document(BytesIO(contents)), job_description)
+        resume_object.compare_keywords()
     else:
         # Indicate fail due to invalid file type
         raise HTTPException(status_code=400, detail="Invalid file type")
--- optimize_resume.py
+++ optimize_resume.py
@@ -1,5 +1,6 @@
 from docx import Document
-from typing import Final
+from enum import Enum
+from typing import Final, Tuple
 from rakun2 import RakunKeyphraseDetector
 
 # Hyperparemeters for rakun2
@@ -10,17 +11,67 @@ RAKUN_HYPERPARAMETERS: Final = {
     "token_prune_len": 3
 }
 
-class Resume():
+class TextAttribute(Enum):
+    resume:str = "resume_text"
+    job:str = "job_description"
+
+class ResumeOptimizer():
     """
     Resume Class - Initialize from a valid python-docx Document object
     """
-    def __init__(self, resume_doc: Document):
+    def __init__(self, resume_doc: Document, job_description: str):
         self.resume_doc = resume_doc
         self.resume_text = "\n".join([p.text for p in self.resume_doc.paragraphs])
+        self.job_description = job_description
         self.__keyword_detector = RakunKeyphraseDetector(RAKUN_HYPERPARAMETERS)
+        self.__keyword_cache = {}
 
-    def get_key_words(self) -> list:
+    def get_keywords(self, attribute: TextAttribute) -> list[Tuple[str, float]]:
         """
-        Extract keywords with rakun2
+        Extract keywords of given attribute (doc name) with rakun2, cache results
+        Args:
+        attribute - TextAttribute class (name = "resume_text" or "job_description")
         """
-        return self.__keyword_detector.find_keywords(self.resume_text, input_type="string")
+        # Get attribute
+        keyword_text = getattr(self, attribute, False)
+        if keyword_text and len(keyword_text) > 0:
+            # Check cache
+            if not self.__keyword_cache[attribute]:
+                # Find keywords and update cache
+                self.__keyword_cache[attribute] = self.__keyword_detector.find_keywords(self.resume_text, input_type="string")
+            # Return cached keywords
+            return self.__keyword_cache[attribute]
+        
+    def compare_keywords(self):
+        """
+        Compare the scores of resume_text and job_description
+        """
+        # Get keywords
+        job_results = self.get_keywords(TextAttribute.job)
+        resume_results = self.get_keywords(TextAttribute.resume)
+        # Create list of job keywords only
+        job_keywords = [keyword_tuple[0] for keyword_tuple in job_results]
+        # Results variables
+        total_matches = 0
+        total_difference = 0
+        match_differences = {}
+        # Iterate over resume keywords
+        for keyword_tuple in resume_results:
+            # Check for a match
+            resume_keyword = keyword_tuple[0]
+            if resume_keyword in job_keywords:
+                # Get matched index
+                matched_index = job_keywords.index(resume_keyword)
+                job_keywords.pop(matched_index)
+                # Get full tuple
+                job_tuple = job_results[matched_index + total_matches]
+                # Record match difference
+                difference = job_tuple[1] - keyword_tuple[1]
+                total_difference += abs(difference)
+                match_differences[resume_keyword] = difference
+                # Update total matches
+                total_matches += 1
+        print(f"Keyword Matches: {total_matches}")
+        print(f"Usage Difference {total_difference}")
+        for keyword, difference in match_differences.items():
+            print(f'Keyword "{keyword}" value difference: {difference}')
