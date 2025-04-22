5bef687e341e9d750f1290f634f32bfe55614d7b
Brady Maki <78286012+bradym05@users.noreply.github.com>
2025-04-21 22:34:42 -0600

    Assertion Fix
    Bug fix for incorrect use of Query

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-from typing import Final, Annotated
-from fastapi import FastAPI, UploadFile, HTTPException, Query
-
-# Constants
-MAX_FILE_SIZE: Final = 256
+from fastapi import FastAPI, UploadFile
 
 # Create FastAPI App Object
 app = FastAPI()
 
 # Main upload function
 @app.post("/uploadfile/")
-async def create_upload_file(file: Annotated[UploadFile, Query(max_length=MAX_FILE_SIZE)]):
+async def create_upload_file(file: UploadFile):
     # Get file contents
     contents = await file.read()
     print(contents)
