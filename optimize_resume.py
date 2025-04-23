627dc34f39346f1f629c05b67cd85f7941eb3023
Brady Maki <78286012+bradym05@users.noreply.github.com>
2025-04-22 23:18:26 -0600

    Resume Parsing
    Seperating resume by sections using common section names

--- optimize_resume.py
+++ optimize_resume.py
@@ -1,11 +1,12 @@
-from docx import Document
+from docx import Document, table
+
 from enum import Enum
-from typing import Final, Tuple, Dict
+from typing import Final, Dict, List
 from rakun2 import RakunKeyphraseDetector
 
 # Hyperparemeters for rakun2
 RAKUN_HYPERPARAMETERS: Final = {
-    "num_keywords": 70,
+    "num_keywords": 20,
     "merge_threshold": 0.5,
     "alpha": 0.9,
     "token_prune_len": 6
@@ -15,79 +16,78 @@ class ResumeOptimizer():
     """
     Resume Class - Initialize from a valid python-docx Document object
     """
+
+    # List of possible resume section names
+    RESUME_SECTIONS = [
+        ["about", "profile", "introduction", "summary", "objective"],
+        ["education", "school"],
+        ["qualification", "skill", "credential", "certification"],
+        ["experience", "history", "project"],
+    ]
+
+    # Sections for comparison
+    COMPARISON_SECTIONS = ["qualification", "experience", "education"]
+
     def __init__(self, resume_doc: Document, job_description: str):
         # Set attributes
         self.resume_doc = resume_doc
         self.job_string = job_description
-        self.resume_string = "\n".join([p.text for p in self.resume_doc.paragraphs])
+        # Parse resume
+        self.__parsed_resume = self.parse_resume()
         # Keywords objects
-        self.resume_keywords = RakunKeyphraseDetector(RAKUN_HYPERPARAMETERS)
-        self.job_keywords = RakunKeyphraseDetector(RAKUN_HYPERPARAMETERS)
+        self.__resume_keywords = RakunKeyphraseDetector(RAKUN_HYPERPARAMETERS)
+        self.__job_keywords = RakunKeyphraseDetector(RAKUN_HYPERPARAMETERS)
 
-    def get_keywords(self, rakun_object:RakunKeyphraseDetector, text:str ) -> list[Tuple[str, float]]:
+    # Seperate resume by sections
+    def parse_resume(self) -> Dict[str, List[str | None]]:
         """
-        Get final keywords from rakun2 object with given text
-        Use pre-existing results if possible
+        Identifies sections, and groups resume content into a dictionary where the keys are the sections
+        Checks for section name matches after blank lines, updates section if there is a match
         """
-        if rakun_object.final_keywords:
-            return rakun_object.final_keywords
-        else:
-            return rakun_object.find_keywords(text, input_type="string")
-        
-    def compare_keywords(self) -> Tuple[Dict[str, Dict[str, float]], list[Tuple[str, float]], float]:
-        """
-        Compare the scores of resume_text and job_description
-        Return values:
-            Match Dictionary {
-                Keyword: {
-                    Difference - Float representing the difference between docs
-                    Value - Float representing the keyword's value in the job posting
-                }
-            }
-            Missed Words - List of missed words sorted from highest to lowest value
-            Total Difference - Float sum of total difference between docs
-        """
-        # Get keywords
-        job_results = self.get_keywords(self.job_keywords, self.job_string)
-        resume_results = self.get_keywords(self.resume_keywords, self.resume_string)
-        # Create list of job keywords only
-        job_keywords = [keyword_tuple[0] for keyword_tuple in job_results]
-        # Results variables
-        matches = {}
-        missed_words = []
-        total_difference = 0
-        # Iterate over resume keywords
-        for keyword_tuple in resume_results:
-            # Check for a match
-            resume_keyword = keyword_tuple[0]
-            if resume_keyword in job_keywords:
-                # Get matched index
-                matched_index = job_keywords.index(resume_keyword)
-                # Get full tuple
-                job_tuple = job_results[matched_index]
-                # Record match difference
-                difference = job_tuple[1] - keyword_tuple[1]
-                total_difference += abs(difference)
-                matches[resume_keyword] = {
-                    "Difference": difference,
-                    "Value": job_tuple[1]
-                }
-        # Iterate over unmatched keywords from job posting
-        for index in range(len(job_keywords)):
-            # Check if keyword matched
-            keyword = job_keywords[index]
-            if keyword in matches.keys():
-                continue
+        # Initialize variables
+        new_section = True
+        current_section = "header"
+        sections = {base_names[0]:[] for base_names in ResumeOptimizer.RESUME_SECTIONS}
+        sections[current_section] = []
+        # Iterate over paragraphs
+        for content in self.resume_doc.iter_inner_content():
+            # Check content type
+            if type(content) == table.Table:
+                # Append all text from all table cells
+                for column in content.columns:
+                    for cell in column.cells:
+                        for paragraph in cell.paragraphs:
+                            # Check if paragraph has content
+                            if paragraph.text.strip():
+                                sections[current_section].append(paragraph.text)
             else:
-                # Update difference
-                total_difference += job_results[index][1]
-                missed_words.append(job_results[index])
-        # Sort missed words
-        missed_words.sort(key=lambda keyword_tuple: keyword_tuple[1], reverse=True)
-        print(f"Usage Difference {total_difference}")
-        for keyword, info in matches.items():
-            print(f'Keyword "{keyword}" value difference: {info["Difference"]}')
-        print("Resume Keywords", [keyword_tuple[0] for keyword_tuple in resume_results])
-        print("Job Keywords", job_keywords)
-        print(missed_words)
-        return matches, missed_words, total_difference
+                # Check for blank line (typically between sections on a resume)
+                if not content.text.strip():
+                    new_section = True
+                    continue
+                elif new_section:
+                    new_section = False
+                    # Iterate over section names
+                    for section_names in ResumeOptimizer.RESUME_SECTIONS:
+                        # Check for a match
+                        if any(name in content.text.lower() for name in section_names):
+                            # Update current section name
+                            current_section = section_names[0]
+                            break
+                # Add text to current section
+                sections[current_section].append(content.text)
+        # Return parsed resume
+        return sections
+    
+    # Compare the keywords of the resume, and job posting
+    def compare_keywords(self):
+        # Get job posting keywords first
+        job_keywords = self.__job_keywords.find_keywords(self.job_string, input_type="string")
+        # Create resume string
+        resume_string = ""
+        for section_name in ResumeOptimizer.COMPARISON_SECTIONS:
+            resume_string += " ".join(self.__parsed_resume[section_name])
+        # Compare keywords using job keywords as priors
+        resume_keywords = self.__resume_keywords.find_keywords(resume_string, input_type="string")
+        print(resume_string)
+        print(resume_keywords)
