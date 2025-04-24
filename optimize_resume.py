c1f593b643e3db037e2ac8fb16727aa747a418b7
Brady Maki <78286012+bradym05@users.noreply.github.com>
2025-04-24 17:14:11 -0600

    Parsing, Headless Chrome
    Created two parsing methods inside of OptimizeResume, each attempts to divide the resume into sections and extract key information.
    
    I am planning on implementing some kind of web scraping to perform live research based on the job posting, and tailor optimizations accordingly, so I updated the build to install a headless chrome browser for linux and add it to the path.

--- optimize_resume.py
+++ optimize_resume.py
@@ -1,6 +1,7 @@
-from docx import Document, table
+import re
 
-from enum import Enum
+from docx import Document, shared
+
 from typing import Final, Dict, List
 from rakun2 import RakunKeyphraseDetector
 
@@ -12,6 +13,9 @@ RAKUN_HYPERPARAMETERS: Final = {
     "token_prune_len": 6
 }
 
+# Regex for contact info
+
+
 class ResumeOptimizer():
     """
     Resume Class - Initialize from a valid python-docx Document object
@@ -20,34 +24,77 @@ class ResumeOptimizer():
     # List of possible resume section names
     RESUME_SECTIONS = [
         ["about", "profile", "introduction", "summary", "objective"],
-        ["education", "school"],
+        ["education", "school", "academic"],
         ["qualification", "skill", "credential", "certification"],
         ["experience", "history", "project"],
     ]
-
-    # Sections for comparison
-    COMPARISON_SECTIONS = ["qualification", "experience", "education"]
+    # Regex for contact info (https://uibakery.io/regex-library/)
+    CONTACT_REGEX = {
+        "phone": "^\\+?\\d{1,4}?[-.\\s]?\\(?\\d{1,3}?\\)?[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,4}[-.\\s]?\\d{1,9}$",
+        "email": r"^\S+@\S+\.\S+$"
+    }
+    URL_REGEX = "^(https?:\\/\\/)?(?:www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-a-zA-Z0-9()@:%_\\+.~#?&\\/=]*)$"
+    # Max length for the title of a section
+    MAX_TITLE_LENGTH = 50
 
     def __init__(self, resume_doc: Document, job_description: str):
-        # Set attributes
+        # Set public attributes
         self.resume_doc = resume_doc
         self.job_string = job_description
-        # Parse resume
-        self.__parsed_resume = self.parse_resume()
+        self.resume_string = "\n".join([p.text for p in self.resume_doc.paragraphs])
+        # Set private attributes
+        self.__missing_contact_info = list(ResumeOptimizer.CONTACT_REGEX.keys())
+        self.__contact_info = {}
+        self.__urls = []
         # Keywords objects
         self.__resume_keywords = RakunKeyphraseDetector(RAKUN_HYPERPARAMETERS)
         self.__job_keywords = RakunKeyphraseDetector(RAKUN_HYPERPARAMETERS)
 
-    # Seperate resume by sections
-    def parse_resume(self) -> Dict[str, List[str | None]]:
+    # Processing for specific sections (accepts text)
+    def __process_header(self, text: str):
+        # Check if any contact info is missing
+        contact_info_count = len(self.__missing_contact_info)
+        if contact_info_count > 0:
+            # Iterate over missing info
+            for index in range(contact_info_count):
+                key = self.__missing_contact_info[index]
+                match = re.search(ResumeOptimizer.CONTACT_REGEX[key], text)
+                if match:
+                    # Update contact info
+                    self.__contact_info[key] = match.string
+                    self.__missing_contact_info.pop(index)
+                    break
+
+    # Update sections dictionary, execute additional operations depending on the section
+    def __update_sections(self, text: str, current_section: str, sections: Dict[str, List[str | None]]):
+        # Update section
+        sections[current_section].append(text)
+        # Call process function (if there is one)
+        match current_section:
+            case "header" | "about":
+                self.__process_header(text)
+        # Get urls from all text
+        url_match = re.search(ResumeOptimizer.URL_REGEX, text)
+        if url_match and not re.match(ResumeOptimizer.CONTACT_REGEX["email"], url_match.string):
+            self.__urls.append(url_match.string)
+
+    # Getters
+    def get_contact_info(self):
+        return self.__contact_info
+    def get_urls(self):
+        return self.__urls
+    
+    # Seperate resume by sections by finding sections from common words
+    def match_parse_resume(self) -> Dict[str, List[str | None]]:
         """
+        [RECOMMENDED PARSE FUNCTION]
         Identifies sections, and groups resume content into a dictionary where the keys are the sections
         Checks for section name matches after blank lines, updates section if there is a match
         """
         # Initialize variables
         new_section = True
         current_section = "header"
-        sections = {base_names[0]:[] for base_names in ResumeOptimizer.RESUME_SECTIONS}
+        sections = {}
         sections[current_section] = []
         # Iterate over paragraphs
         for paragraph in self.resume_doc.paragraphs:
@@ -64,20 +111,45 @@ class ResumeOptimizer():
                     if any(name in paragraph.text.lower() for name in section_names):
                         # Update current section name
                         current_section = section_names[0]
+                        sections[current_section] = []
                         content_paragraph = False
                         break
             if content_paragraph:
-                # Add text to current section
-                sections[current_section].append(paragraph.text)
+                # Update current section
+                self.__update_sections(paragraph.text, current_section, sections)
         # Return parsed resume
         return sections
-    
+
+    # Seperate resume by sections by finding section names with a different font size
+    def font_parse_resume(self) -> Dict[str, List[str | None]]:
+        """
+        Identifies sections, and groups resume content into a dictionary where the keys are the sections.
+        Checks for paragraphs with a font that differs from the rest of the document, or has a style name
+        which contains "Heading".
+        """
+        # Initialize variables
+        current_section = "header"
+        sections = {}
+        sections[current_section] = []
+        # Iterate over paragraphs
+        for paragraph in self.resume_doc.paragraphs:
+            # Check if paragraph text is within max length AND: style name is Heading, font size differs
+            if (len(paragraph.text) <= ResumeOptimizer.MAX_TITLE_LENGTH and 
+                (paragraph.style.name.startswith("Heading") or 
+                 any(run.font.size != None for run in paragraph.runs))):
+                # Check if text was 
+                # Update section
+                current_section = paragraph.text
+                sections[current_section] = []
+            else:
+                # Update current section
+                self.__update_sections(paragraph.text, current_section, sections)
+        return sections
+        
     # Compare the keywords of the resume, and job posting
     def compare_keywords(self):
-        # Create resume string
-        resume_string = "\n".join([p.text for p in self.resume_doc.paragraphs])
         # Get keywords
-        resume_keywords = self.__resume_keywords.find_keywords(resume_string, input_type="string")
+        resume_keywords = self.__resume_keywords.find_keywords(self.resume_string, input_type="string")
         job_keywords = self.__job_keywords.find_keywords(self.job_string, input_type="string")
         # Find matching keywords
         matches = {}
